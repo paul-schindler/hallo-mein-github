c0e0b600cda1f9857af51e5c1e7b921e1a509c7b
paul-schindler <paul_schindler@gmx.at>
2019-05-01 19:44:02 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hallo_mein_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+              Console.WriteLine("Hello World! "+ args[0]);
+            
+            else
+              Console.WriteLine("Hello World ohne Argumente!");
+        } 
+    }
+}
